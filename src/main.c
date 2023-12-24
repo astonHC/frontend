200c9ece8a3f6544667a5cae2c8654058f2fcb63
Harry Clark <139020900+astonHC@users.noreply.github.com>
2023-12-24 13:23:06 +0000

    initial commits

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/* COPYRIGHT (C) HARRY CLARK */
+
+/* MY FRONTEND FOR GUI APPS IN C */
+
+/* THIS FILE PERTAINS TOWARDS THE MAIN FUNCTIONALITY OF */
+/* THE FRONTEND */
+
+/* NESTED INCLUDES */
+
+#include "SDL-BASE.h"
+
+int main(int argc, char** argv)
+{
+    #undef USE_SDL
+
+    return 0;
+}
